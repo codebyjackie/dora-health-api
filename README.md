81eff587722d685f877eb247fbd26df75acda164
codebyjackie <JackieLinDev@foxmail.com>
2024-06-03 20:50:50 -0500

    chore: Initial commit
    - Initialized project directory
    - Configured various configuration files

---
+++ README.md
@@ -0,0 +1,1 @@
+# dora-health-api
---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import nodePlugin from 'eslint-plugin-node';
+
+export default [
+  {
+    ignores: ['.gitignore'],
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      ecmaVersion: 'latest',
+      globals: globals.node,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      node: nodePlugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 80,
+          trailingComma: 'none',
+          endOfLine: 'auto',
+        },
+      ],
+      'no-undef': 'error',
+      'node/no-missing-require': 'off',
+      'node/no-unpublished-require': 'off',
+    },
+  },
+];
